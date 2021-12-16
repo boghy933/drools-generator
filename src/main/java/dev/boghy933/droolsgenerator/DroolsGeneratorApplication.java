f52b928b585e52dbd7c661e156654a90e2e9078e
boghy933 <boghy933@gmail.com>
2021-12-16 22:15:10 +0100

    Starter empty project

---
+++ src/main/java/dev/boghy933/droolsgenerator/DroolsGeneratorApplication.java
@@ -0,0 +1,13 @@
+package dev.boghy933.droolsgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroolsGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroolsGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/boghy933/droolsgenerator/DroolsGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.boghy933.droolsgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroolsGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
